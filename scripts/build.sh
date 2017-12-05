02ebfe897ce0334ae2fef3603a6e1839f95804f1
Bartosz Maciaszek <bartosz.maciaszek@gmail.com>
2017-12-05 23:03:59 +0100

    using xdebug-2.6.0alpha1

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env sh
 
-apk --update add nginx
+apk --update add autoconf make gcc libc-dev nginx git
 
-./install-composer.sh
+EXPECTED_COMPOSER_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_COMPOSER_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit
+fi
+
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer --quiet
+
+rm composer-setup.php
 
 composer global require hirak/prestissimo
+
+docker-php-ext-install pdo_mysql json opcache
+
+pecl install xdebug-2.6.0alpha1
+
+docker-php-source delete
+
+apk del autoconf gcc libc-dev
